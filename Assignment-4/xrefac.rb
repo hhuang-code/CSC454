dd1c9465ebd2836fd906ad61fbee66b7ee7b5dcc
huanghao-code <huanghao_bupt@sina.com>
2017-11-09 16:09:44 -0500

    Update xrefac.rb

--- Assignment-4/xrefac.rb
+++ Assignment-4/xrefac.rb
@@ -1,11 +1,13 @@
-# --
+# In the following code, pc means program counter 
+
 require 'ostruct'
 require 'set'
 require 'erb'
 
-def has_pc(file_hash, pc)
-	file_hash.each do |key, value|
-		value.each do |line_info|
+# Check whether the given program counter is listed in debug_info
+def has_src(file_hash, pc)
+	file_hash.each do |filename, debug_line_arr|
+		debug_line_arr.each do |line_info|
 			if Integer(line_info.pc) == Integer('0x' + pc)
 				final_tuple = OpenStruct.new
 				final_tuple.pc = pc
@@ -16,12 +18,38 @@ def has_pc(file_hash, pc)
 			end	
 		end
 	end
+	# Not found
 	final_tuple = OpenStruct.new
 	final_tuple.pc = pc
 	final_tuple.filename = nil
 	final_tuple.row = nil
 	final_tuple.gray = nil
 	return false, final_tuple 
+end
+
+# Check whether a given row in the source file has corresponding assembly
+def has_instr(file_hash, filename, row)
+	debug_line_arr = file_hash[filename]
+	debug_line_arr.each do |line_info|
+		if Integer(row) == Integer(line_info.row)
+			if line_info.pc != '0xffffffff'
+				return true
+			else
+				return false
+			end
+		end
+	end
+	return false
+end
+
+# Check whether a given row in the source file has been added to final_arr
+def added_final_arr(final_arr, filename, row)
+	final_arr.each do |final_tuple|
+		if final_tuple.filename == filename && Integer(row) == Integer(final_tuple.row)
+			return true
+		end
+	end
+	return false
 end
 
 if __FILE__ == $0
@@ -37,7 +65,7 @@ if __FILE__ == $0
 	# An array to store .debug_line info
 	debug_line_arr = Array.new
 	# Regex to match valid source file name
-	file_re = /((\/.*[\w\-. ]+\.cp*))/i
+	file_re = /(\/.*[\w\-. ]+\.[c|h])/i
 	# Regex to capture pc and line number
 	line_re = /(^0x.*)(\[\s*)(\d*)(.*,\s*)(\d*)(\s*\])/i
 	# Regex to capture pc and assembly instructions
@@ -48,7 +76,6 @@ if __FILE__ == $0
 	# Parse the output of dwarfdump
 	filename = ''
 	is_fun = false
-	line_cnt = 1
 	file_hash = Hash.new
 	func_set = Set.new
 	IO.popen(['dwarfdump' , exe_name]){
@@ -76,7 +103,9 @@ if __FILE__ == $0
 			if file_match = line.match(file_re)
 				filename = file_match.captures
 				filename = filename.at(0).strip
-				file_hash[filename] = Array.new
+				if !file_hash.has_key?(filename)
+					file_hash[filename] = Array.new
+				end
 				line_cnt = 1
 			end
 			line_info.filename = filename
@@ -84,52 +113,90 @@ if __FILE__ == $0
 			line_info.pc = line_info.pc.strip
 			line_info.row = line_info.row.strip
 			line_info.col = line_info.col.strip
-			while line_cnt < Integer(line_info.row) do
-				single_info = OpenStruct.new
-				single_info.pc = '0xffffffff'
-				single_info.row = line_cnt.to_s
-				single_info.col = '0'
-				single_info.filename = filename
-				file_hash[filename].push(single_info)
-				line_cnt += 1
-			end
 			# Add line info to an array
 			file_hash[filename].push(line_info)
-			if line_cnt == Integer(line_info.row)
-				line_cnt += 1
-			end
 		end
 		}
 	}
 
 	# Traverse the output of dwarfdump to decide which line should be printed in gray color
-	file_hash.each do |key, value|
-		for i in 0..value.size - 1
+	file_hash.each do |filename, debug_line_arr|         
+		for i in 0..debug_line_arr.size - 1
 			is_found = false
-			for j in 0..i - 1
-				if value.at(j).row == value.at(i).row
+            for j in 0..i - 1
+				if debug_line_arr.at(j).row == debug_line_arr.at(i).row
 					is_found = true
 					break
 				end
 			end
 			if is_found
-				value.at(i).gray = true
+				debug_line_arr.at(i).gray = true
 			else
-				value.at(i).gray = false
+				debug_line_arr.at(i).gray = false
 			end
 		end
 	end
 
+	# Sort each file according to row number, for inserting source code without assembly
+	file_hash.each do |filename, debug_line_arr|
+		file_hash[filename] = file_hash[filename].sort_by{|line_info| Integer(line_info.row)}
+	end
+
+	# Insert source code without assembly
+	file_hash.each do |filename, debug_line_arr|
+		file = File.open(filename, 'r')
+		total_src_line = file.readlines.size
+		total_debug_line = debug_line_arr.size
+		src_line_cnt = 1
+		# Insert source code
+		for i in 0..total_debug_line - 1
+			while src_line_cnt < Integer(file_hash[filename].at(i).row)
+				single_info = OpenStruct.new
+				single_info.pc = '0xffffffff'
+				single_info.row = src_line_cnt.to_s
+				single_info.col = '0'
+				single_info.filename = filename
+				single_info.gray = false
+				file_hash[filename].push(single_info)
+				src_line_cnt += 1
+			end
+			if src_line_cnt == Integer(file_hash[filename].at(i).row)
+				src_line_cnt += 1
+			end
+		end
+		# Insert the remaining source code
+		while src_line_cnt <= total_src_line
+			single_info = OpenStruct.new
+			single_info.pc = '0xffffffff'
+			single_info.row = src_line_cnt.to_s
+			single_info.col = '0'
+			single_info.filename = filename
+			single_info.gray = false
+			file_hash[filename].push(single_info)
+			src_line_cnt += 1
+		end	
+		file_hash[filename] = file_hash[filename].sort_by{|line_info| Integer(line_info.row)}
+	end
+
 	# Traverse the output of dwarfdump to decide which instruction is generated for multiply source lines
 	pc_set = Set.new
-	file_hash.reverse_each do |key, value|
-		value.reverse_each do |line_info|
+	file_hash.reverse_each do |filename, debug_line_arr|
+		debug_line_arr.reverse_each do |line_info|
+			# Only the final source line will have assembly instructions
 			if pc_set.include? (line_info.pc)
 				line_info.pc = '0xffffffff'
 			else
 				pc_set.add(line_info.pc)
 			end
 		end
+	end
+
+	puts '---------------------------- debug_line ----------------------------'
+	file_hash.each do |file, debug_line|
+		debug_line.each do |line_info|
+			puts line_info.pc + ' ' + line_info.row + ' ' + line_info.col + ' ' + ' ' + line_info.gray.to_s + ' ' + line_info.filename
+		end
+		puts "\n"
 	end
 	
 	# Parse the output of objdump
@@ -172,44 +239,49 @@ if __FILE__ == $0
 		}
 	}
 
-	# Combine the output of objdump and that of dwarfdump, a hashmap to keep track of the counter to dwarfdump
-	# Key is filaname, value is a counter to file_hash, and initially set to 0
-	dwarf_cnt_hash = Hash.new
-	file_hash.each do |key, value|
-		dwarf_cnt_hash[key] = 0
+	puts '---------------------------- instructions ----------------------------'
+	instr_hash.each do |program_cnter, ass_code|
+		puts program_cnter + ' ' + ass_code
 	end
+	puts "\n"
 
 	# The final output index array. Its elements are tuples: (pc, instruction, file, line_num, flag)
 	final_arr = Array.new
-	instr_hash.each do |key, value|
-		if func_set.include? (key)
+	instr_hash.each do |program_cnter, ass_code|
+		if func_set.include? (program_cnter)
 			final_tuple = OpenStruct.new
-			final_tuple.pc = key
+			final_tuple.pc = program_cnter
 			final_tuple.instr = nil
 			final_tuple.filename = nil
 			final_tuple.row = nil
 			final_tuple.gray = nil
 			final_arr.push(final_tuple)
 		else
-			has_src, final_tuple = has_pc(file_hash, key)
-			final_tuple.instr = value
-			# Has corresponding source code
-			if has_src
-				while file_hash[final_tuple.filename].at(dwarf_cnt_hash[final_tuple.filename]).pc == '0xffffffff' do
-					new_final_tuple = OpenStruct.new
-					new_final_tuple.pc = nil
-					new_final_tuple.instr = nil
-					new_final_tuple.filename = final_tuple.filename
-					new_final_tuple.row = file_hash[final_tuple.filename].at(dwarf_cnt_hash[final_tuple.filename]).row
-					new_final_tuple.gray = false
-					final_arr.push(new_final_tuple)
-					dwarf_cnt_hash[final_tuple.filename] += 1
+			has_src_res, final_tuple = has_src(file_hash, program_cnter)
+			final_tuple.instr = ass_code
+			# The instruction has corresponding source code
+			if has_src_res
+				# Insert previous source code which does not have assembly
+				prev_row = Integer(final_tuple.row) - 1
+				stack_arr = Array.new
+				while prev_row >= 1 && !has_instr(file_hash, final_tuple.filename, prev_row)
+					# Haven't add to final_arr
+					if !added_final_arr(final_arr, final_tuple.filename, prev_row)
+						new_final_tuple = OpenStruct.new
+						new_final_tuple.pc = nil
+						new_final_tuple.instr = nil
+						new_final_tuple.filename = final_tuple.filename
+						new_final_tuple.row = prev_row.to_s
+						new_final_tuple.gray = false
+						stack_arr.push(new_final_tuple)
+					end
+					prev_row -= 1
 				end
-				if Integer(file_hash[final_tuple.filename].at(dwarf_cnt_hash[final_tuple.filename]).pc) == Integer('0x' + final_tuple.pc)
-					final_arr.push(final_tuple)
-					dwarf_cnt_hash[final_tuple.filename] += 1
+				while !stack_arr.empty?
+					final_arr.push(stack_arr.pop)
 				end
-			# No corresponding source code
+				final_arr.push(final_tuple)
+			# The instruction has no corresponding source code
 			else
 				final_arr.push(final_tuple)
 			end
@@ -219,17 +291,41 @@ if __FILE__ == $0
 	# Read in all source files, and store them in a hashmap
 	# Key is file name, and value is an array, each element is a line.
 	file_content_hash = Hash.new
-	file_hash.each do |key, value|
+	file_hash.each do |filename, _|
 		# Read in each source file
 		source_content_arr = Array.new
-		File.open(key, 'r') do |file|
+		File.open(filename, 'r') do |file|
 			# Read in each line
 			file.each_line do |line|
+				# Change to HTML escape
+				line = CGI::escapeHTML(line)
+				# Replace whitespace with '&nbsp;'
+				line_length = line.size
+				space_num = 0
+				tab_num = 0
+				idx = 0
+				for i in 0..line_length - 1
+					if line[i] == ' '
+						space_num += 1
+					elsif line[i] == '\t'
+						tab_num += 1
+					else
+						idx = i
+						break
+					end
+				end
+				# Truncate non-whitespace part
+				line = line[idx, line_length]
+				# Add leading whitespace in HTML format
+				for i in 1..2 * space_num + 4 * tab_num
+					line = '&nbsp;' + line
+				end
+				puts line
 				source_content_arr.push(line)
 			end
 			file.close
 		end
-		file_content_hash[key] = source_content_arr	
+		file_content_hash[filename] = source_content_arr	
 	end
 
 	# Add source file line content to final_tuple
@@ -242,10 +338,11 @@ if __FILE__ == $0
 	end
 
 	# Add subroutine jump link
+	jump_re = /(^callq|jmp|jo|jno|js|jns|je|jz|jne|jnz|jb|jnae|jc|jnb|jae|jnc|jbe|jna|ja|jnbe|jl|jnge|jge|jnl|jle|jng|jg|jnle|jp|jpe|jnp|jpo|jcxz|jecxz)(\s+)([0-9a-z]+)(\s*)/i
 	final_arr.each do |final_tuple|
 		if final_tuple.instr != nil
 			# A subroutine call
-			if call_match = final_tuple.instr.match(/(^callq)(\s+)([0-9a-z]+)(\s*)/i)
+			if call_match = final_tuple.instr.match(jump_re)
 				callq, space1, dest, space2 = call_match.captures
 				dest = dest.strip
 				final_tuple.target = dest
@@ -269,5 +366,4 @@ if __FILE__ == $0
 	final_arr.each do |final_tuple|
 		puts final_tuple
 	end
-	
 end
