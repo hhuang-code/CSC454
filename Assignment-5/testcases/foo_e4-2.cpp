26e5e3e14778e817424daa678ce18e8f0b29eb87
huanghao-code <huanghao_bupt@sina.com>
2017-11-22 05:40:00 -0500

    Add files via upload

--- Assignment-5/testcases/foo_e4-2.cpp
+++ Assignment-5/testcases/foo_e4-2.cpp
@@ -28,30 +28,31 @@ int main(int argc, char **argv)
 	}
 
     // subscripting
-    cout << "Print elements:" << endl;
+    cout << "Print 12 elements:" << endl;
 	for(int i = 0; i < 12; i++){
-		cout << foo[i] << endl;
+		cout << "("<< (i + 1) << ")-th element: " << foo[i] << endl;
 	}
 
 	cout << endl;
 
     cout << "Test add or subtract an integer:" << endl;
     // Print the first element: 100
-	cout << *foo << endl;
+	cout << "(1)-th element: " << *foo << endl;
 
+    cout << "Minus the second element by 10." << endl;
     // Modify the second element to 91
 	*(foo + 1) -= 10;
 
     // Print the second element: 91
-	cout << *(foo + 1) << endl;
+	cout << "(2)-th element: " << *(foo + 1) << endl;
 
     // Print the first element: still 100
-	cout << *foo << endl;
+	cout << "(1)-th element: " << *foo << endl;
 
     Pointer<int, false> bar;
     bar = foo + 3;
     // Print the fourth element: 103
-    cout << *bar << endl;
+    cout << "(4)-th element: " << *bar << endl;
 
     cout << endl;
 
@@ -61,14 +62,14 @@ int main(int argc, char **argv)
 	fun = foo + 4;
 
     // foo is smaller than fun, print: false (0)
-	cout << (foo > fun) << endl; 
+	cout << "foo > foo + 4 true or false? " << (foo > fun) << endl; 
 
     Pointer<int, false> bat;   // default constructor
 
     bat = fun - 1;
 
     // bat is small than fun, print: true (1)
-    cout << (bat < fun) << endl;
+    cout << "fun - 1 < fun true or false? " << (bat < fun) << endl;
 
     bat.~Pointer();
     bar.~Pointer();
