c7098db55a564ea9e0195202d040b16ddef5688f
huanghao-code <huanghao_bupt@sina.com>
2017-11-21 22:37:50 -0500

    Update foo7.cpp

--- Assignment-5/testcases/foo7.cpp
+++ Assignment-5/testcases/foo7.cpp
@@ -16,21 +16,26 @@ void error(const char *text)
 
 void sub(Pointer<int> &foo)
 {
-    Pointer<int> bar(foo);
-    Pointer<int> bat(bar);
-    Pointer<int> qix(bat);
-    Pointer<int> glorch(qix);
+    Pointer<int> bar(foo);	// copy constructor
+    Pointer<int> bat(bar);	// copy constructor
+    Pointer<int> qix(bat);	// copy constructor
+    Pointer<int> glorch(qix);	// copy constructor
 
-    *glorch = 100;
+    *glorch = 100;	// foo, bar, bat, qix and glorch point to the same tombstone
     if (*foo != 100)
 		error("Linking of pointers not correct!");
 
-    free(glorch);
+	//bar.~Pointer();
+	//bat.~Pointer();
+	//qix.~Pointer();
+	//glorch.~Pointer();
+	//foo.~Pointer();
+    free(glorch);	// dangling pointer. To avoid this, you should call arbitrary four destructors. If five are called, memory leak will happen.
 }
 
 int main(int argc, char **argv)
 {
-    Pointer<int> foo(new int(0));
+    Pointer<int> foo(new int(0));	// bootstrapping constructor
     sub(foo);
     *foo = 1000;
     error("Didn't complain about use of dangling pointer foo!");
